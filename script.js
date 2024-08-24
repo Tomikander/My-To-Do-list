c9ce8a271ba1483cc3bcf0b9e24c8e091fec1fa1
Tomikander <167113924+Tomikander@users.noreply.github.com>
2024-08-24 13:39:20 +0300

    script.js

--- script.js
+++ script.js
@@ -8,17 +8,45 @@ document.addEventListener('DOMContentLoaded', () => {
        button.disabled = value.length < 3 || value.length > 200;
    }
 
-  
    input.addEventListener('input', checkInput);
 
-  
    button.addEventListener('click', () => {
-       if (input.value.trim()) {
-           const li = document.createElement('li');
-           li.textContent = input.value.trim();
-           todoList.appendChild(li);
-           input.value = '';
-           checkInput();
-       }
-   });
+      if (input.value.trim()) {
+         const li = document.createElement('li');
+
+         const checkbox = document.createElement('input');
+         checkbox.type = 'checkbox';
+         checkbox.className = 'checkbox';
+         li.appendChild(checkbox);
+
+         const text = document.createElement('span');
+         text.textContent = input.value.trim();
+         li.appendChild(text);
+
+         const editButton = document.createElement('button');
+         editButton.textContent = 'edit';
+         editButton.className = 'edit';
+         editButton.addEventListener('click', () => {
+            const newValue = promt('Редактировать задачу:', text.textContent);
+            if (newValue !== null) {
+               text.textContent = newValue.trim();
+            }
+         });
+         li.appendChild(editButton);
+
+         const deleteButton = document.createElement('button');
+         deleteButton.textContent = 'delete';
+         deleteButton.className = 'delete';
+         deleteButton.addEventListener('click', () => {
+            todoList.removeChild(li);
+         });
+         li.appendChild(deleteButton);
+
+         todoList.appendChild(li);
+
+         input.value = '';
+         checkInput();
+      }
+   })
+   
 });
